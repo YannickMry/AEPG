6323c4879ef7fca0a15104edd035c6eff5be0d69
YannickMry <maury.yannick@gmail.com>
2021-04-18 00:18:51 +0200

    ArticleType form added

---
+++ src/Form/ArticleType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Intitulé',
+            ])
+            ->add('image')
+            ->add('content', TextareaType::class,  [
+                'label' => 'Contenu',
+            ])
+            ->add('isHidden', CheckboxType::class,  [
+                'label' => 'Voulez-vous cacher l\'article au public ?',
+            ])
+            ->add('author', TextType::class,  [
+                'label' => 'Auteur(s)',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
