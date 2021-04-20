1e57b653d4ca3949eafc25469660737254bf0510
YannickMry <maury.yannick@gmail.com>
2021-04-20 11:30:13 +0200

    Ajout de webpack, bootstrap et jquery

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,16 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/app.scss';
+
+import { Tooltip, Toast, Popover } from 'bootstrap';
+import $ from 'jquery';
 
 // start the Stimulus application
 import './bootstrap';
+
+console.log('Made with ❤️');
+
+$(function(){
+    $('h1').css("color", "green");
+});
